d8ac246e4a20d4129d9dab2fcb6f5106d3d3606a
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-20 19:41:52 -0500

    removes logs, comments, and unused code

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+read -p "Do you want to stage all files? [Y/N]" stage
+if true; then # stage all files
+	if git status; then
+		echo "THIS BITCH RANNNNNNNNNN"
+		git add -A 
+	else 
+		echo "Failed to make a cleanup commit because this is not a git repository"; exit 
+	fi
+else
+	echo "FUCK THIS FUCKING THING"
+fi
+
 if git status; then
 	if git commit -m 'removes logs, comments, and unused code'; then
 		git push
@@ -10,3 +22,5 @@ if git status; then
 else
 	echo "Failed to make a cleanup commit because this is not a git repository"
 fi
+
+exit 
