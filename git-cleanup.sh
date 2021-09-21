0310cb5580b275fbc96d41b91dcc15f7eeab2422
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-20 19:50:02 -0500

    sets up prompt to stage all files

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-read -p "Do you want to stage all files? [Y/N]" stage
-if true; then # stage all files
-	if git status; then
-		echo "THIS BITCH RANNNNNNNNNN"
-		git add -A 
+read -p "Do you want to stage all files? [ Y / N ] " stage
+	if (false); then # stage all files
+		if git status; then
+			echo "Staging all files with `git add -A`"
+			git add -A 
+		else 
+			echo "Failed to make a cleanup commit because this is not a git repository"; exit 
+		fi
 	else 
-		echo "Failed to make a cleanup commit because this is not a git repository"; exit 
+		echo "No files were staged"
 	fi
-else
-	echo "FUCK THIS FUCKING THING"
-fi
 
 if git status; then
 	if git commit -m 'removes logs, comments, and unused code'; then
