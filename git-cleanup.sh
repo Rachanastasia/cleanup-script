ef3df3f74a4715c8bf2ed6f6c90335fa03f5f500
Rachel Reilly <r.a.reill18@gmail.com>
2021-09-20 20:04:33 -0500

    removes logs, comments, and unused code

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-read -p "Do you want to stage all files? [ Y / N ] " stage
-	if (false); then # stage all files
-		if git status; then
+if git status; then
+	git status
+	
+	read -p "Do you want to stage all files? [ Y / N ] " stage
+		if (true); then # stage all files
 			echo "Staging all files with `git add -A`"
 			git add -A 
 		else 
-			echo "Failed to make a cleanup commit because this is not a git repository"; exit 
+			echo "Files were not staged"
 		fi
-	else 
-		echo "No files were staged"
-	fi
 
-if git status; then
 	if git commit -m 'removes logs, comments, and unused code'; then
 		git push
 		echo "Pushed cleanup commit to repository"
 	else
 		echo "Failed to make a cleanup commit because there are no staged files"
 	fi
-else
-	echo "Failed to make a cleanup commit because this is not a git repository"
-fi
-
-exit 
+else 
+	echo "Failed to make a cleanup commit because this is not a git repository"; exit 
+fi; exit
